f86a5cc7ad2cc22c1dfd1eb7c232425441ef28e4
ro14hit <rohit1499singh@gmail.com>
2024-04-11 18:20:04 +0530

    Added add user form

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { useNavigate } from 'react-router-dom';
-import { Button } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 const Navigation = () => {
   const [value, setValue] = React.useState(0);
@@ -9,14 +9,38 @@ const Navigation = () => {
 
   return (
     <div className="fixed top-12 w-full  text-center">
-    <Box className="bg-yellow-200 flex justify-center" >
-      <Button onClick={()=>{navigate('/home');}}>Home</Button>
-      <Button onClick={()=>{navigate('/addUser');}}>Add User</Button>
-      <Button onClick={()=>{navigate('/deleteUser');}}>Delete User</Button>
-      <Button onClick={()=>{navigate('/updateUser');}}>Update User</Button>
-    </Box>
+      <Box className="bg-yellow-200 flex flex-wrap justify-center">
+        <Button
+          onClick={() => {
+            navigate("/home");
+          }}
+        >
+          Home
+        </Button>
+        <Button
+          onClick={() => {
+            navigate("/addUser");
+          }}
+        >
+          Add User
+        </Button>
+        <Button
+          onClick={() => {
+            navigate("/deleteUser");
+          }}
+        >
+          Delete User
+        </Button>
+        <Button
+          onClick={() => {
+            navigate("/updateUser");
+          }}
+        >
+          Update User
+        </Button>
+      </Box>
     </div>
   );
-}
+};
 
-export default Navigation
+export default Navigation;
--- client/src/pages/AddUser.jsx
+++ client/src/pages/AddUser.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React from "react";
+import Grid from "@mui/material/Grid";
 
 const AddUser = () => {
-  return (
-    <div className='flex justify-center items-center h-60'>
-      AddUser
-    </div>
-  )
-}
+  const submitFun = (e) => {
+    e.preventDefault();
+    console.log("submit fun clicked");
+  };
 
-export default AddUser
+  return (
+    <div className="flex flex-wrap justify-center m-16 items-center h-60">
+      <form onSubmit={submitFun}>
+        <Box className="border mt-10 rounded-md border-black p-4 flex flex-col">
+          <Typography
+            textAlign={"center"}
+            variant="h6"
+            padding={2}
+            className="text-red-500"
+          >
+            Add User Form
+          </Typography>
+          <Grid container spacing={2} className="mb-5">
+            <Grid item xs={6}>
+              <TextField
+                required
+                label="First Name"
+                autoComplete="off"
+                fullWidth
+              ></TextField>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Middle Name"
+                autoComplete="off"
+                fullWidth
+              ></TextField>
+            </Grid>
+            <Grid item xs={6}>
+              {" "}
+              <TextField
+                required
+                label="Last Name"
+                autoComplete="off"
+                fullWidth
+              ></TextField>
+            </Grid>
+            <Grid item xs={6}>
+              {" "}
+              <TextField
+                required
+                label="Gender"
+                autoComplete="off"
+                fullWidth
+              ></TextField>
+            </Grid>
+          </Grid>
+          <div className="flex justify-center">
+            <Button type="submit" className="" variant="contained">
+              Submit
+            </Button>
+          </div>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
